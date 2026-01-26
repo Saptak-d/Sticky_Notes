09871ae3009a8fc5dbb59f3535795f36df293f73
saptak dutta <duttasaptak79@gmail.com>
2026-01-26 23:31:47 +0530

    changes of logic fix bug and started this project for next 1 week

--- src/index.js
+++ src/index.js
@@ -1,10 +1,8 @@
 import app from "./app.js"
-import dotenv from "dotenv"
+import "dotenv/config"; 
 import connectDB from "./db/index.js";
 
-dotenv.config({
-    path : "../.env"
-})
+
 const PORT  = process.env.PORT || 8080;
 
 connectDB()
