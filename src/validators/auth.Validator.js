9b5a8e901136bac4059c3653d786b74265764649
saptak dutta <duttasaptak79@gmail.com>
2026-02-14 18:23:46 +0530

    change the userRegistrationValidator logic

---
+++ src/validators/auth.Validator.js
@@ -0,0 +1,36 @@
+import { body } from "express-validator";
+
+const userRegistrationValidator = () => {
+  return [
+    body("email")
+      .trim()
+      .notEmpty().withMessage("Email is required")
+      .isEmail().withMessage("Email is invalid"),
+
+    body("username")
+      .trim()
+      .notEmpty().withMessage("Username is required")
+      .isLength({ min: 3 }).withMessage("Username should be at least 3 characters")
+      .isLength({ max: 13 }).withMessage("Username can't exceed 13 characters"),
+
+    body("password")
+      .trim()
+      .notEmpty().withMessage("Password is required")
+      .isLength({ min: 4 }).withMessage("Password should be at least 4 characters")
+      .isLength({ max: 13 }).withMessage("Password can't exceed 13 characters"),
+      
+      body("fullname")
+      .trim()
+      .notEmpty().withMessage("The Full Name is required ")
+
+  ];
+};
+
+const userLoginValidator = () => {
+  return [
+    body("email").isEmail().withMessage("Email is not valid"),
+    body("password").notEmpty().withMessage("Password can't be empty"),
+  ];
+};
+
+export { userRegistrationValidator, userLoginValidator };
