48065f9a88dd855bbe9d3612b46fe78af6870d19
saptak dutta <duttasaptak79@gmail.com>
2026-03-24 22:04:48 +0530

    setup the router for deleteProject and setup

--- src/routes/project.routes.js
+++ src/routes/project.routes.js
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import {createProject,getProjectsById,updateProject,deleteProject} from "../controllers/project.controllers.js"
-import {createProjectValidator,getProjectsByIdValidator,updateProjectValidator} from "../validators/project.Validator.js"
+import {createProjectValidator,getProjectsByIdValidator,updateProjectValidator,deleteProjectValidator} from "../validators/project.Validator.js"
 import {validator} from "../middlewares/validator.middleware.js"
 import {verifyJWT} from "../middlewares/auth.middleware.js"
 
@@ -9,7 +9,7 @@ const router = Router()
 router.route("/createProject").post(verifyJWT,createProjectValidator(),validator,createProject)
 router.route("/getProjectsById/:projectId").get(verifyJWT,getProjectsByIdValidator(),validator,getProjectsById);
 router.route("/updateProject/:projectId").patch(verifyJWT,updateProjectValidator(),validator,updateProject)
-router.route("/")
+router.route("/deleteProject/:projectId").delete(verifyJWT,deleteProjectValidator(),validator,deleteProject)
 
 
  export default router; 
