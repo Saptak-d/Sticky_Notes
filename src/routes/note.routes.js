4f166fcd68329d24d9a3bb1b7244c2173d53938b
saptak dutta <duttasaptak79@gmail.com>
2026-04-11 14:53:16 +0530

    create the router for the notes route and setup it

--- src/routes/note.routes.js
+++ src/routes/note.routes.js
@@ -3,13 +3,14 @@ import { createNote } from "../controllers/note.controllers";
 import {verifyJWT ,validateProjectPermission} from "../middlewares/auth.middleware"
 import {validator} from "../middlewares/validator.middleware"
 import {AvailableUserRoles, UserRolesEnum} from "../utils/constants"
+import {createNoteValidator} from "../validators/note.Validator"
 
 const router = Router()
 
 router.use(verifyJWT)
 
 router.route(":/projectId")
-    .post(validateProjectPermission([UserRolesEnum.ADMIN]),)
+    .post(validateProjectPermission([UserRolesEnum.ADMIN]),createNoteValidator(),validator,createNote)
 
 
 
