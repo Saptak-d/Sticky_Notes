d7e63bc8b26c029c96eee83750ad77c8ab4bdc19
saptak dutta <duttasaptak79@gmail.com>
2026-04-15 14:58:38 +0530

    create the createTaskValidator

--- src/routes/task.routes.js
+++ src/routes/task.routes.js
@@ -1,7 +1,11 @@
 import { Router } from "express";
+import {createTask } from "../controllers/task.controllers.js";
+import {upload} from "../middlewares/multer.middleware.js"
+import {validator} from "../middlewares/validator.middleware.js"
 
 
 const router = Router()
+
 
 
 export default router;
