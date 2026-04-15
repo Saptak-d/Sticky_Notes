1db30f6dbd7823d9343e0e7bcbf7d112e6baa599
saptak dutta <duttasaptak79@gmail.com>
2026-04-15 15:10:41 +0530

    create the router for createTask and setup it

--- src/routes/task.routes.js
+++ src/routes/task.routes.js
@@ -1,10 +1,24 @@
 import { Router } from "express";
-import {createTask } from "../controllers/task.controllers.js";
 import {upload} from "../middlewares/multer.middleware.js"
 import {validator} from "../middlewares/validator.middleware.js"
+import {verifyJWT} from "../middlewares/auth.middleware.js"
+import {validateProjectPermission} from "../middlewares/auth.middleware.js"
+import { UserRolesEnum } from "../utils/constants.js";
+import {createTask } from "../controllers/task.controllers.js";
+import {createTaskValidator} from "../validators/taskValidator.js"
+
 
 
 const router = Router()
+router.use(verifyJWT)
+
+router.route("/:projectId")
+   .post(
+     validateProjectPermission([UserRolesEnum.ADMIN , UserRolesEnum.PROJECT_ADMIN]),
+     createTaskValidator(),
+     validator,
+     createTask,
+   )
 
 
 
